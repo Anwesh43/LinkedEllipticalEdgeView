457b41b9d427c5012dada0341f29da8431b53397
Anwesh43 <anweshthecool0@gmail.com>
2019-02-03 12:53:47 +0530

    adding EllipticalEdge class

--- ellipticaledgeview/src/main/java/com/anwesh/uiprojects/ellipticaledgeview/EllipticalEdgeView.kt
+++ ellipticaledgeview/src/main/java/com/anwesh/uiprojects/ellipticaledgeview/EllipticalEdgeView.kt
@@ -192,4 +192,28 @@ class EllipticalEdgeView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class EllipticalEdge(var i : Int) {
+
+        private val root : EENode = EENode(0)
+        private var curr : EENode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
