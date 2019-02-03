7bfb148aaf65aab523b4ae2e83e85675bc16b251
Anwesh43 <anweshthecool0@gmail.com>
2019-02-03 11:56:34 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedellipticaledgeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedellipticaledgeview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedellipticaledgeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.ellipticaledgeview.EllipticalEdgeView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view : EllipticalEdgeView = EllipticalEdgeView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
