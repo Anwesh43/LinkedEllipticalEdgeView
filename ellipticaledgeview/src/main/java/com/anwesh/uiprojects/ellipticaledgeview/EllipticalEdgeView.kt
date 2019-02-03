837626f851781ea6e3f907a2d5f829bb15469f1d
Anwesh43 <anweshthecool0@gmail.com>
2019-02-03 12:20:31 +0530

    adding constants

--- ellipticaledgeview/src/main/java/com/anwesh/uiprojects/ellipticaledgeview/EllipticalEdgeView.kt
+++ ellipticaledgeview/src/main/java/com/anwesh/uiprojects/ellipticaledgeview/EllipticalEdgeView.kt
@@ -13,6 +13,15 @@ import android.graphics.Path
 import android.app.Activity
 import android.content.Context
 
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val nodes : Int = 5
+val lines : Int = 4
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.8f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val foreColor : Int = Color.parseColor("#EF6C00")
+
 fun Canvas.drawWelcomeText(paint : Paint) {
     val textSizeFactor : Int = 8
     val welcomeText : String = "W.E.L.C.O.M.E"
@@ -26,9 +35,9 @@ fun Canvas.drawWelcomeText(paint : Paint) {
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
-fun Float.scaleFactor() : Float = Math.floor(this / 0.51).toFloat()
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
-fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * 0.05f
+fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
 class EllipticalEdgeView(ctx : Context) : View(ctx) {
 
@@ -59,7 +68,7 @@ class EllipticalEdgeView(ctx : Context) : View(ctx) {
     data class State(var scale : Float = 0f, var prevScale : Float = 0f, var dir : Float = 0f) {
 
         fun update(cb : (Float) -> Unit) {
-            scale += scale.updateValue(dir, 4, 1)
+            scale += scale.updateValue(dir, lines, 1)
             if (Math.abs(scale - prevScale) > 1) {
                 scale = prevScale + dir
                 dir = 0f
