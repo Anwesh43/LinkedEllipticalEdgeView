2225727faf18dcb2010952f473891db1040af8a6
Anwesh43 <anweshthecool0@gmail.com>
2019-02-03 11:38:04 +0530

    adding libraries

--- ellipticaledgeview/src/main/java/com/anwesh/uiprojects/ellipticaledgeview/EllipticalEdgeView.kt
+++ ellipticaledgeview/src/main/java/com/anwesh/uiprojects/ellipticaledgeview/EllipticalEdgeView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.ellipticaledgeview
 /**
  * Created by anweshmishra on 03/02/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.Path
+import android.app.Activity
+import android.content.Context
