1ed5cf5a28feef86c8f06037f6a4b33bb72cd09b
Anwesh43 <anweshthecool0@gmail.com>
2019-02-03 11:42:57 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedellipticaledgeview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedellipticaledgeview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedellipticaledgeview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.ellipticaledgeview.EllipticalEdgeView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val view : EllipticalEdgeView = EllipticalEdgeView.create(this)
     }
 }
