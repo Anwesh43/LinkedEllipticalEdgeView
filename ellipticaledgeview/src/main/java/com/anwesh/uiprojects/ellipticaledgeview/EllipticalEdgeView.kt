f080cb853cfa8e1aa81e9d43a259b6e4d1e3006c
Anwesh43 <anweshthecool0@gmail.com>
2019-02-03 11:47:29 +0530

    drawing welcome text

--- ellipticaledgeview/src/main/java/com/anwesh/uiprojects/ellipticaledgeview/EllipticalEdgeView.kt
+++ ellipticaledgeview/src/main/java/com/anwesh/uiprojects/ellipticaledgeview/EllipticalEdgeView.kt
@@ -13,12 +13,21 @@ import android.graphics.Path
 import android.app.Activity
 import android.content.Context
 
+fun Canvas.drawWelcomeText(paint : Paint) {
+    val textSizeFactor : Int = 15
+    val welcomeText : String = "W.E.L.C,O.M.E"
+    drawColor(Color.CYAN)
+    paint.color = Color.WHITE
+    paint.textSize = Math.min(width, height).toFloat() / textSizeFactor
+    val tw : Float = paint.measureText(welcomeText)
+    drawText(welcomeText,  width / 2 - tw / 2, height / 2 - paint.textSize / 4, paint)
+}
 class EllipticalEdgeView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
     override fun onDraw(canvas : Canvas) {
-
+        canvas.drawWelcomeText(paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
@@ -35,7 +44,7 @@ class EllipticalEdgeView(ctx : Context) : View(ctx) {
         fun create(activity : Activity) : EllipticalEdgeView {
             val view : EllipticalEdgeView = EllipticalEdgeView(activity)
             activity.setContentView(view)
-            return view 
+            return view
         }
     }
 }
